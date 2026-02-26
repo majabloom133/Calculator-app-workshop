1ca44fb55a01857a614865af57775646082d8f9f
Maja <maja.bloom@outlook.com>
2026-02-26 10:47:31 +0100

    Add try-catch to Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -9,22 +9,24 @@ public class Calculator {
         boolean keepRunning = true;
 
         while (keepRunning) {
-            System.out.println("--- Calculator ---");
-
-            // This is where I will add the math
-            // Ask for first number, ask for operator, ask for second number
-            System.out.print("Enter the first number: ");
-            double num1 = reader.nextDouble();
-
-            System.out.print("Enter operator (+, -, *, /): ");
-            String operator = reader.next();
-
-            System.out.print("Enter the second number: ");
-            double num2 = reader.nextDouble();
-
-            // Add switch
             double result = 0;
 
+            // Add try to make sure program doesn't crash if user adds a number that's not valid
+            try {
+                System.out.println("--- Calculator ---");
+
+                // This is where I will add the math
+                // Ask for first number, ask for operator, ask for second number
+                System.out.print("Enter the first number: ");
+                double num1 = reader.nextDouble();
+
+                System.out.print("Enter operator (+, -, *, /): ");
+                String operator = reader.next();
+
+                System.out.print("Enter the second number: ");
+                double num2 = reader.nextDouble();
+
+            // Add switch
             switch (operator) {
                 case "+":
                     result = num1 + num2;
@@ -48,6 +50,12 @@ public class Calculator {
                     break;
                 default:
                     System.out.println("Invalid operator! Please use +, -, * or /.");
+            }
+
+            // Add catch to give an optional action if user types in wrong numbers
+            } catch (Exception e) {
+                System.out.println("Please enter valid numbers.");
+                reader.next();
             }
 
             // Ask if user wants to end program
