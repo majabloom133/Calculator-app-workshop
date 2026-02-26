9b8d969f5c6fe6fda6b2160af87623a691c3ff8e
Maja <maja.bloom@outlook.com>
2026-02-26 11:12:18 +0100

    Add JUnit dependency to pom.xml, create test class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        // Creates test, checks if 5 + 5 = 10
+        double expected = 10.0;
+        double actual = 5.0 + 5.0;
+
+        // Checks if both values are the same
+        assertEquals(expected, actual, "Addition calculation failed!");
+    }
 }
