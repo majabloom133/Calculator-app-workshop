15d6ac7de2615d6f81eff0c1c843f376c2672ba2
Maja <maja.bloom@outlook.com>
2026-02-26 10:34:49 +0100

    Add changes to some faults

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,6 +12,43 @@ public class Calculator {
             System.out.println("--- Calculator ---");
 
             // This is where I will add the math
+            // Ask for first number, ask for operator, ask for second number
+            System.out.print("Enter the first number: ");
+            double num1 = reader.nextDouble();
+
+            System.out.print("Enter operator (+, -, *, /): ");
+            String operator = reader.next();
+
+            System.out.print("Enter the second number: ");
+            double num2 = reader.nextDouble();
+
+            // Add switch
+            double result = 0;
+
+            switch (operator) {
+                case "+":
+                    result = num1 + num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                        System.out.println("Result: " + result);
+                    } else {
+                        System.out.println("Error: Division by zero is not allowed!");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator! Please use +, -, * or /.");
+            }
 
             // Ask if user wants to end program
             System.out.println("Do you want to end program? (yes/no)");
@@ -21,6 +58,6 @@ public class Calculator {
                 System.out.println("Goodbye!");
             }
             }
-
+        reader.close();
         }
     }
