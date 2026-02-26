b5f42e8ece5e5ba1fad787bf59a99d5eafcdcb9e
Maja <maja.bloom@outlook.com>
2026-02-26 11:40:39 +0100

    Complete JUnit tests for the four operators

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,4 +13,28 @@ public class CalculatorTest {
         // Checks if both values are the same
         assertEquals(expected, actual, "Addition calculation failed!");
     }
+
+    @Test
+    public void testSubtraction() {
+        // Will check if 20 - 8 = 12
+        double expected = 12.0;
+        double actual = 20.0 - 8.0;
+        assertEquals(expected, actual, "Subtraction calculation failed!");
+    }
+
+    @Test
+    public void testMultiplication() {
+        // Will check if 5 * 4 = 20
+        double expected = 20.0;
+        double actual = 5.0 * 4.0;
+        assertEquals(expected, actual, "Multiplication calculation failed!");
+    }
+
+    @Test
+    public void testDivision() {
+        // Will check if 10 / 2 = 5
+        double expected = 5.0;
+        double actual = 10.0 / 2.0;
+        assertEquals(expected, actual, "Divison calculation failed!");
+    }
 }
